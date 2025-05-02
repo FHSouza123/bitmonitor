34705c0021a493e7bf6a2406cbbc6ba7d41dd96f
Fabiano Henrique de Souza <fabian.230@hotmail.com>
2025-05-02 01:49:54 -0300

    refactor: atualiza configuração do Vite e scripts de build

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1024, // Ajuste o limite conforme necessário
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false
   },
   server: {
-    host: true, // Permite acesso externo
-    port: 3000, // Porta padrão
-    open: true, // Abre o navegador automaticamente
-  },
-  preview: {
-    port: 3000,
-    host: true
+    port: 3000
   }
 })
